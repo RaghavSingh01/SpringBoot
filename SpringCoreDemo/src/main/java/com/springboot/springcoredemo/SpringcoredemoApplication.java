3dde146b82ccd21fa53648d7a1b3ebcca3c53042
Raghav <singh.raghav1013@gmail.com>
2026-08-21 00:31:33 +0530

    Dependency Injection and Component Scanning

--- SpringCoreDemo/src/main/java/com/springboot/springcoredemo/SpringcoredemoApplication.java
+++ SpringCoreDemo/src/main/java/com/springboot/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,13 @@ package com.springboot.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	For this i moved coach interface and its implementing class cricketCoach from common folder in this package to a seperate package named utils thats why i had to tell springboot 
+	to scan that package too because by default it ignores that package. Without this explicit mention of scanning different packages we would need to keep everything in the same package.
+@SpringBootApplication(
+	scanBasePackages= {"com.springboot.springcoredemo",
+						"com.springboot.util"}
+)*/
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
--- SpringCoreDemo/src/main/java/com/springboot/springcoredemo/rest/DemoController.java
+++ SpringCoreDemo/src/main/java/com/springboot/springcoredemo/rest/DemoController.java
@@ -1,8 +1,10 @@
-package com.springboot.springcoredemo;
+package com.springboot.springcoredemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.springcoredemo.common.Coach;
 
 @RestController
 public class DemoController {
@@ -10,7 +12,7 @@ public class DemoController {
     //Define a private field for the dependency
     private Coach myCoach;
 
-    //Define a constructor for dependency injection. Autowired is optional here because we only have one constructor here. 
+    //Define a constructor for dependency injection. Autowired is optional here because we only have one constructor here. theCoach is a dependency or helper being injected here.
     @Autowired
     public  DemoController(Coach theCoach){
         myCoach = theCoach;
